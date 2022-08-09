990866812b8fb999e1471f047907b8e4b8352a71
IcemanMick <doylewithad@gmail.com>
2022-08-09 19:25:47 +0000

    set up header tags on base.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Topic
 
-# Create your views here.
+
+class TopicList(generic.ListView):
+    model = Topic
+    queryset = Topic.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 3
