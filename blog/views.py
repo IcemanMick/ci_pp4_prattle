01930cbe18221eeb79c0a8c6cfed8fb22b128491
IcemanMick <doylewithad@gmail.com>
2022-08-10 19:40:21 +0000

    link thread list.html to index.html correctly

--- blog/views.py
+++ blog/views.py
@@ -15,15 +15,15 @@ class ThreadList(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Topic.objects.filter(status=1)
         topic = get_object_or_404(queryset, slug=slug)
-        comments = topic.comments.filter(approved=True).order_by('-created_on')
-        liked = False
-        if topic.likes.filter(id=self.request.user.id).exists():
-            liked = True
+        threads = topic.threads.filter(approved=True).order_by('-created_on')
+        # liked = False
+        # if topic.likes.filter(id=self.request.user.id).exists():
+            # liked = True
 
         return render(
             request, "thread_list.html", {
                 "post": topic,
-                "comments": comments,
-                "liked": liked
+                "threads": threads,
+                # "liked": liked
                 },
         )
