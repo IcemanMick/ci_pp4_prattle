c5af3dcd896f6e0eb2a40f96b33c69a65ae1aa93
IcemanMick <doylewithad@gmail.com>
2022-08-10 00:20:39 +0000

    start creating the ThreadDetail view

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.TopicList.as_view(), name="home")
+    path('', views.TopicList.as_view(), name="home"),
+    path('<slug:slug>/', views.ThreadDetail.as_view(), name='thread_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Topic
 
 
@@ -8,3 +8,21 @@ class TopicList(generic.ListView):
     queryset = Topic.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 3
+
+
+class ThreadDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Topic.objects.filter(status=1)
+        thread = get_object_or_404(queryset, slug=slug)
+        comments = thread.comments.filter(approved=True).order_by('-created_on')
+        liked = False
+        if thread.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(request, 'thread.html', {
+            "post": thread,
+            "comments": comments,
+            "liked": liked
+        }
+        )
